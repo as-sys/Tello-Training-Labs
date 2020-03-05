da1c307f4a55d98525b87fb002cab261c2809749
Andrew Gowan <andrew_847@icloud.com>
2020-03-05 10:47:25 -0800

    Updated Triangle Function Timing #2

--- TelloTrainingLabs.py
+++ TelloTrainingLabs.py
@@ -51,7 +51,7 @@ def square():
 
 # Triangle Function
 def triangle():
-    sendmsg('up 100')
+    sendmsg('up 50')
     for i in range(3):
         sendmsg('forward 100') #Drone will go forward 100cm
         sendmsg('ccw 120') #Drone will turn Counter Clockwise 120 degrees
@@ -71,7 +71,8 @@ try:
         sendmsg('command', 0)
         sendmsg('takeoff' ,8)
 
-        square() #calling funciton square
+        #square() #calling funciton square
+        triangle()
 
         sendmsg('land')
 
